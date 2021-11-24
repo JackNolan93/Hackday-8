364b3887e3a04b41ad04affdfe57ffed67eb3045
Leon P-S <leon.ps@focusrite.com>
2021-11-24 09:24:49 +0000

    Initial project commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <juce_core/juce_core.h>
+#include <iostream>
+
+int main (int argc, char* argv[])
+{
+  juce::ignoreUnused(argc, argv);
+
+    // Your code goes here!
+    //juce::ignoreUnused (argc, argv);
+
+    //juce::AudioBuffer<float> buffer;
+
+    std::cout << "TEST" << std::endl;
+    return 0;
+}
